147877b37e841e0022abe0ab9b41d30e6b3a2678
hankaj <hanna.jarlaczynska@gmail.com>
2021-01-01 10:36:01 +0100

    Append additional functions for binary trees

--- drzewa/main.c
+++ drzewa/main.c
@@ -6,6 +6,7 @@ struct el_drzewa{
     struct el_drzewa *lewy, *prawy;
 };
 
+
 void wstaw(struct el_drzewa **pocz, double war){
     struct el_drzewa *nowy, *ws=*pocz;
     nowy=malloc(sizeof(struct el_drzewa));
@@ -26,12 +27,14 @@ void wstaw(struct el_drzewa **pocz, double war){
     }
 }
 
+
 void wypisz(struct  el_drzewa *pocz){
     if (pocz==0) return;
     wypisz(pocz->lewy);
-    printf(" %lf", pocz->w);
+    printf("%lf d", pocz->w);
     wypisz(pocz->prawy);
 }
+
 
 struct el_drzewa *znajdz(struct  el_drzewa *pocz, double war){
     struct el_drzewa *wynik, *wynik2;
@@ -41,19 +44,65 @@ struct el_drzewa *znajdz(struct  el_drzewa *pocz, double war){
     if (wynik) return wynik;
 
     wynik2=znajdz(pocz->prawy, war);
-    if (wynik2) return wynik2;
-    return 0;
+    return wynik2;
+}
+
+
+int max_wysokosc(struct el_drzewa *pocz){
+    int wynik1, wynik2;
+    if (pocz==0) return 0;
+    wynik1=1+max_wysokosc(pocz->lewy);
+    wynik2=1+max_wysokosc(pocz->prawy);
+    if (wynik1>wynik2) return wynik1;
+    else return wynik2;
+}
+
+
+int min_wysokosc(struct el_drzewa *pocz){
+    int wynik1, wynik2;
+    if (pocz==0) return 0;
+    wynik1=1+min_wysokosc(pocz->lewy);
+    wynik2=1+min_wysokosc(pocz->prawy);
+    if (wynik1<wynik2) return wynik1;
+    else return wynik2;
+}
+
+
+void przesun(struct  el_drzewa *pocz, struct el_drzewa **koniec, int wysokosc){
+    if (pocz->lewy==0) {*koniec=pocz; return;}
+    if (pocz->prawy==0) {*koniec=pocz; return;}
+    if (wysokosc){
+        przesun(pocz->lewy, koniec, wysokosc-1);
+        przesun(pocz->prawy, koniec, wysokosc-1);
+    }
+}
+
+
+void dopisz(struct el_drzewa *pocz, double war){
+    struct el_drzewa *nowy, *koniec=0;
+    int wysokosc;
+
+    nowy=malloc(sizeof(struct el_drzewa));
+    nowy->w = war;
+    nowy->lewy = nowy->prawy = 0;
+
+    wysokosc = min_wysokosc(pocz);
+    przesun(pocz, &koniec,wysokosc-1);
+    if (koniec->lewy==0) koniec->lewy=nowy;
+    else koniec->prawy=nowy;
 }
 
 
 int main() {
     struct el_drzewa *d=0, *d2;
-    wstaw(&d, 5);
-    wstaw(&d, 11);
     wstaw(&d, 4);
-    wstaw(&d, 10);
-    wstaw(&d, 1);
     wstaw(&d, 8);
+    wstaw(&d, 2);
+    wstaw(&d, 12);
+    wstaw(&d, 10);
+    wstaw(&d, 5);
+    wstaw(&d, 1);
+
     wypisz(d);
     printf("\n");
     d2 = znajdz(d, 5);
